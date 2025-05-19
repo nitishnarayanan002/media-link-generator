1e2a7014106f92d388c592f22b015f10d0090e54
Nitishnarayanann <digitalgadgetinfo@gmail.com>
2025-05-19 16:08:41 +0530

    Update index.js
    app.js updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // add `.jsx` explicitly
+import App from './App.js'; // add `.jsx` explicitly
 import './index.css'; // if you're using a global CSS file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
