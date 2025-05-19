379367fce56ec7d7f6e6f2dae602ee9bdc3c55b1
Nitishnarayanann <digitalgadgetinfo@gmail.com>
2025-05-19 17:22:24 +0530

    Update index.js
    Have made the changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'; // add `.jsx` explicitly
-import './index.css'; // if you're using a global CSS file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
